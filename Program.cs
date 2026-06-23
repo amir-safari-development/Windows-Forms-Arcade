fc200777446a6c4a7bb2f4fc3499597cc00a95ca
RINO TEK <Sinasinkogo@gmail.com>
2026-06-24 01:06:45 +0330

    this is my second commit

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace Arcade_Game
         static void Main()
         {
             //this is just for the test of the repository , done by SINAM
+            //this is gonna be the second test by SINAM , still a lot of stuff TO DO !!
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
